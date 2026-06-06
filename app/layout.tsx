21723cd1ddc1bbeb3fa7e07ecfe4b91f8ae6dc79
Zyvrra <xolanisinethemba9@gmail.com>
2026-06-07 00:17:05 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,4 @@
-import "./globals.css"
+
 import { CONFIG } from "@/lib/config"
 export const metadata = { title: CONFIG.APP_NAME, description: "SA social marketplace" }
 export default function RootLayout({ children }: { children: React.ReactNode }) {
